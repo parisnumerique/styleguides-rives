4790c0a6f77647277978428bc9ca778b4fca94d1
Hans Lemuet <hans@etaminstudio.com>
2017-03-20 23:04:16 +0100

    list-item: support for soundcloud embed

--- src/modules/list-item/_script.js
+++ src/modules/list-item/_script.js
@@ -10,12 +10,15 @@ GFrds.listItem = (function(){
     var $el = $(selector),
       $buttonToggle,
       $embed,
-      $image;
+      $image,
+      $iframe,
+      soundcloud;
 
     function init(){
       $embed = $el.find('.gfrds_list-item-embed');
       $buttonToggle = $el.find('.gfrds_list-item-button-toggle');
       $image = $embed.find('img');
+      $iframe = $embed.find('iframe');
 
       $el.on('click', onClick);
       $buttonToggle.on('click', onClickButtonToggle);
@@ -23,29 +26,65 @@ GFrds.listItem = (function(){
 
     function onClick(e){
       e.preventDefault();
-
-      if ($el.hasClass('is-open')) return;
-      $el.addClass('is-open');
-
-      setTimeout(function(){
-        var img = new Image();
-        img.onload = function () {
-          $image.attr('src', $image.data('large'));
-        }
-        img.src = $image.data('large');
-        $el.velocity('scroll', {
-          offset: -60
-        });
-      }, 600);
-
-      resetAllItems();
+      open();
     }
 
     function onClickButtonToggle(e){
       e.preventDefault();
       if (!$el.hasClass('is-open')) return;
       e.stopPropagation();
+      close();
+    }
+
+    function open() {
+      if ($el.hasClass('is-open')) return;
+      $el.addClass('is-open');
+
+      setTimeout(isOpened, 600);
+      resetAllItems();
+    }
+
+    function isOpened() {
+      if ($iframe.length) {
+        enableEmbed();
+      } else if ($image.data('large')) {
+        loadLargeImage();
+      }
+
+      $el.velocity('scroll', {
+        offset: -60
+      });
+    }
+
+    function close() {
+      if ($iframe.length) {
+        disableEmbed();
+      }
+
       $el.removeClass('is-open');
+    }
+
+    function enableEmbed() {
+      if (soundcloud) {
+        $iframe.show();
+        soundcloud.play();
+      } else {
+        $iframe.attr('src', $iframe.data('original'));
+        soundcloud = SC.Widget($iframe[0]);
+      }
+    }
+
+    function disableEmbed() {
+      if (soundcloud) {soundcloud.pause();}
+      $iframe.hide();
+    }
+
+    function loadLargeImage() {
+      var img = new Image();
+      img.onload = function () {
+        $image.attr('src', $image.data('large'));
+      }
+      img.src = $image.data('large');
     }
 
     function resetAllItems() {
