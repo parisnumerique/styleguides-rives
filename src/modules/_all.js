77d5ddad4dca2ecf9812083d0c63d6579de21b6d
Hans Lemuet <hans@etaminstudio.com>
2017-03-21 12:53:14 +0100

    cover: added video player, closing #33

--- src/modules/_all.js
+++ src/modules/_all.js
@@ -1,4 +1,5 @@
 require('./anchor/_script');
+require('./cover/_script');
 require('./list-item/_script');
 require('./navbar/_script');
 require('./ribbons/_script');
---
+++ src/modules/cover/_script.js
@@ -0,0 +1,88 @@
+'use strict';
+
+require('velocity-animate');
+
+var GFrds = window.GFrds || {};
+
+GFrds.cover = (function(){
+
+  function module(selector){
+    var $el = $(selector),
+      $link,
+      $embed,
+      $iframe,
+      $backgroundVideo;
+
+    function init(){
+      $link = $el.find('.gfrds_cover-link');
+      $embed = $el.find('.gfrds_cover-embed');
+      $iframe = $embed.find('iframe');
+      $backgroundVideo = $el.find('.gfrds_cover-video');
+
+      var embedHeight = $el.height() - 200;
+      var embedWidth = embedHeight * 2.38;
+      $embed.css({
+        height: embedHeight,
+        width: embedWidth
+      });
+
+      $el.on('click', onClick);
+      $link.on('click', onClickLink);
+    }
+
+    function onClick(e){
+      e.preventDefault();
+      close();
+    }
+
+    function onClickLink(e){
+      e.preventDefault();
+      if ($el.hasClass('is-open')) return;
+      e.stopPropagation();
+      open();
+    }
+
+    function open() {
+      if ($el.hasClass('is-open')) return;
+      $el.addClass('is-open');
+
+      $backgroundVideo[0].pause();
+      setTimeout(isOpened, 600);
+    }
+
+    function isOpened() {
+      if ($iframe.length) {
+        enableEmbed();
+      }
+    }
+
+    function close() {
+      $el.removeClass('is-open');
+      $backgroundVideo[0].play();
+      disableEmbed();
+    }
+
+    function enableEmbed() {
+      $iframe.attr('src', $iframe.data('original'));
+    }
+
+    function disableEmbed() {
+      $iframe.attr('src', '');
+    }
+
+    init();
+
+    return $el;
+  }
+
+  return function(selector){
+    return $(selector).each(function(){
+      module(this);
+    });
+  };
+
+})();
+
+$(document).ready(function(){
+  GFrds.cover('.gfrds_cover');
+});
