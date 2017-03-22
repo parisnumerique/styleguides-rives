b623bab0ef662b421d267efa823fbce723f7134a
Lilit Miskaryan <lilit.rande@paris.fr>
2017-03-22 14:54:16 +0100

    [#embed-video] styles, fixies

--- src/javascript/_features/lazyload.js
+++ src/javascript/_features/lazyload.js
@@ -2,7 +2,7 @@ var LazyLoad = require('vanilla-lazyload');
 
 $(document).ready(function(){
   var lazyloader = new LazyLoad({
-    elements_selector: '.gfrds_push-embed iframe, .gfrds_portrait-embed iframe, .gfrds_figure-image',
+    elements_selector: '.gfrds_video-embed iframe, .gfrds_push-embed iframe, .gfrds_portrait-embed iframe, .gfrds_figure-image',
     throttle: 1000
   });
 });
